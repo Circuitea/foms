6aae8188ad7300fd93fa5ef215f60cbac9b4b9f6
DJCAYZ <charlesaaron07@gmail.com>
2025-05-15 03:01:56 +0800

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -82,7 +82,11 @@ export default {
     				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
     				border: 'hsl(var(--sidebar-border))',
     				ring: 'hsl(var(--sidebar-ring))'
-    			}
+    			},
+				header: {
+					DEFAULT: 'hsl(var(--header))',
+					foreground: 'hsl(var(--header-foreground))',
+				}
     		}
     	}
     },
