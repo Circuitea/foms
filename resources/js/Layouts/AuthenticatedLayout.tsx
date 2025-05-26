797407f058bd2252e64ab0b9321a2ff4af6cb929
Alexis Laniosa <alexis.laniosa@my.jru.edu>
2025-05-26 15:07:05 +0800

    header changes

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from '@/components/app-sidebar';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { PropsWithChildren } from 'react';
+import ApplicationLogo from '@/components/ApplicationLogo';
 
 export default function Authenticated({
     children,
@@ -9,8 +10,11 @@ export default function Authenticated({
         <SidebarProvider>
             <AppSidebar />
             <main className='w-full'>
-                <div className='bg-header text-header-foreground w-full flex justify-center p-8 text-4xl font-black'>
-                    <p>CITY DISASTER RISK REDUCTION AND MANAGEMENT OFFICE</p>
+                <div className='bg-[#1B2560] text-white w-full flex items-center gap-2 p-4 border-b-4 border-red-500'>
+                    <ApplicationLogo className='h-20 w-20 rounded-full ml-2' />
+                    <p className='font-black tracking-wide w-full' style={{ fontFamily: 'Montserrat, sans-serif', fontSize: '39px', textAlign: 'center'}} >
+                        CITY RISK REDUCTION AND MANAGEMENT OFFICE
+                    </p>
                 </div>
                 {children}
             </main>
