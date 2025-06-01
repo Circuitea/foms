7fb934475a536e9adad3fbb41595fa9d6d700de2
DJCAYZ <charlesaaron07@gmail.com>
2025-06-01 13:29:28 +0800

    change user name in sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -72,7 +72,7 @@ export function AppSidebar() {
                 </div>
               </div>
               <div className="text-center">
-                <p className="text-white font-medium text-lg">Yuan</p>
+                <p className="text-white font-medium text-lg">{user.first_name}</p>
                 <p className="text-white/70 text-sm">Admin</p>
               </div>
             </div>
