4c74dba42f8fb774b08362c9e73c6d0c4d93d474
DJCAYZ <charlesaaron07@gmail.com>
2025-06-04 17:10:00 +0800

    fix department_id column not being dropped

--- database/migrations/2025_06_01_105914_add_department_id_to_roles.php
+++ database/migrations/2025_06_01_105914_add_department_id_to_roles.php
@@ -23,6 +23,7 @@ return new class extends Migration
     {
         Schema::table('roles', function (Blueprint $table) {
             $table->dropForeign(['department_id']);
+            $table->dropColumn('department_id');
         });
     }
 };
