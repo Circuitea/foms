ac583fe9f697bf0bda3a438682201f46ff3ae589
DJCAYZ <charlesaaron07@gmail.com>
2025-06-18 02:44:42 +0800

    add seeders for default meeting type values

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Personnel;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Personnel::factory(10)->create();
-
-        Personnel::factory()->create([
-            'first_name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            MeetingTypeSeeder::class,
         ]);
     }
 }
