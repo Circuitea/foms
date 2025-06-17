59e14fad14c3655c64facee4fda98667a8746a7c
DJCAYZ <charlesaaron07@gmail.com>
2025-06-18 02:46:55 +0800

    add agendas relation to meeting model

--- app/Models/Meeting.php
+++ app/Models/Meeting.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\MeetingPriority;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Meeting extends Model
@@ -37,6 +38,11 @@ class Meeting extends Model
     public function format(): MorphTo
     {
         return $this->morphTo(__FUNCTION__, 'format_type', 'format_id');
+    }
+
+    public function agendas(): HasMany
+    {
+        return $this->hasMany(MeetingAgenda::class. 'meeting_id');
     }
 
 
--- app/Models/MeetingAgenda.php
+++ app/Models/MeetingAgenda.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MeetingAgenda extends Model
 {
     public $timestamps = false;
+    protected $fillable = ['order', 'agenda'];
+
+    public function meeting(): BelongsTo
+    {
+        return $this->belongsTo(Meeting::class, 'meeting_id');
+    }
 }
