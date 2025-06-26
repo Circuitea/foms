4e07190ea9c4746190d42256dcffd2ca6711c380
DJCAYZ <charlesaaron07@gmail.com>
2025-06-26 17:05:33 +0800

    add inventory models and related relationships

---
+++ app/Models/Inventory/ItemType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Inventory;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ItemType extends Model
+{
+    protected $table = 'inventory_item_types';
+    public $timestamps = false;
+
+    protected $fillable = ['name'];
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class, 'type_id');
+    }
+}
