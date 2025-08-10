97dabc191ec46216a3bf9343eb16b29c26bebb59
DJCAYZ <charlesaaron07@gmail.com>
2025-08-10 18:00:21 +0800

    add api endpoint for verifying tokens and updating locations

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,36 @@
 <?php
 
+use App\Events\LocationUpdated;
+use App\Http\Resources\PersonnelLocationResource;
 use App\Models\Personnel;
+use App\Models\PersonnelLocation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
+use Laravel\Sanctum\PersonalAccessToken;
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
+    });
+
+    Route::post('/location', function (Request $request) {
+        $user = $request->user();
+
+        Log::info('New location update', [
+            'user' => $user->id,
+            'lat' => $request['latitude'],
+            'lng' => $request['longitude'],
+        ]);
+
+        PersonnelLocation::upsert([
+            ['latitude' => $request['latitude'], 'longitude' => $request['longitude'], 'id' => $user->id],
+        ], uniqueBy: ['id'], update: ['latitude', 'longitude']);
+
+        LocationUpdated::dispatch(PersonnelLocationResource::collection(PersonnelLocation::all()));
+
     });
 });
 
@@ -28,4 +50,20 @@ Route::post('/login', function (Request $request) {
     }
 
     return $user->createToken($request->device_name)->plainTextToken;
+});
+
+Route::post('/verify-token', function (Request $request) {
+    Log::info('New token verification request sent.', [
+        'token' => $request->bearerToken(),
+    ]);
+
+    if (!$request->bearerToken()) {
+        return response()->json(['valid' => false], 401);
+    }
+
+    $token = PersonalAccessToken::findToken($request->bearerToken());
+    
+    return response()->json([
+        'valid' => $token !== null,
+    ]);
 });
