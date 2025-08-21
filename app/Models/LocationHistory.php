2c6b036141281869cc9a1f9d08949965b32b62e9
DJCAYZ <charlesaaron07@gmail.com>
2025-08-21 18:46:28 +0800

    add location history handling
    user interface pending

---
+++ app/Models/LocationHistory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LocationHistory extends Model
+{
+  protected $fillable = ['latitude', 'longitude'];
+
+
+  protected function casts() {
+        return [
+            'latitude' => 'decimal:8',
+            'longitude' => 'decimal:8',
+        ];
+    }
+
+  public function personnel(): BelongsTo
+  {
+    return $this->belongsTo(Personnel::class, 'personnel_id');
+  }
+}
