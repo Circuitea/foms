bf517af73c3d9d56637bda79d98e73f1014956b7
DJCAYZ <charlesaaron07@gmail.com>
2025-10-01 05:11:48 +0800

    add missing emergency color in personnel list

--- resources/js/Pages/Personnel/ListPersonnel.tsx
+++ resources/js/Pages/Personnel/ListPersonnel.tsx
@@ -39,6 +39,7 @@ const getStatusColor = (status: Status | null) => {
       'available': "bg-green-100 text-green-800",
       'assigned': "bg-blue-100 text-blue-800",
       'on leave': "bg-yellow-100 text-yellow-800",
+      'emergency': 'bg-red-100 text-red-800',
     }
     return colors[status];
   }
