9ff35934ed2c2b26639fe167779684e9c215280a
DJCAYZ <charlesaaron07@gmail.com>
2025-10-01 04:17:40 +0800

    add emergency as status option

--- app/Status.php
+++ app/Status.php
@@ -7,6 +7,7 @@ enum Status: string
     case AVAILABLE = 'available';
     case ASSIGNED = 'assigned';
     case ON_LEAVE = 'on leave';
+    case EMERGENCY = 'emergency';
 
     public function label(): string
     {
@@ -14,6 +15,7 @@ enum Status: string
             static::AVAILABLE => 'Available',
             static::ASSIGNED => 'Assigned Task',
             static::ON_LEAVE => 'On Leave',
+            static::EMERGENCY => 'In Emergency',
         };
     }
 }
