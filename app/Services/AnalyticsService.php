c876fe8ce0497dff130656a312cc8a81b1a8f23c
DJCAYZ <charlesaaron07@gmail.com>
2025-10-07 15:31:04 +0800

    remove caching on analytics service results

--- app/Services/AnalyticsService.php
+++ app/Services/AnalyticsService.php
@@ -16,20 +16,17 @@ class AnalyticsService
   }
 
   public function getRecommendation(string $id, string $model_ident, mixed $date, mixed $quantities) {
-    $cache_key = "consumable_item_recommendation:" . $id;
-    return Cache::remember($cache_key, now()->endOfMonth(), function () use ($model_ident, $date, $quantities) {
-      $url = $this->serviceURL . "/recommendations/" . $model_ident . "/make";
-      $response = Http::acceptJson()
-        ->post($url, [
-          'year' => $date->year,
-          'month' => $date->month,
-          'quarter' => $date->quarter,
-          'quantities' => $quantities,
-        ]);
+    $url = $this->serviceURL . "/recommendations/" . $model_ident . "/make";
+    $response = Http::acceptJson()
+      ->post($url, [
+        'year' => $date->year,
+        'month' => $date->month,
+        'quarter' => $date->quarter,
+        'quantities' => $quantities,
+      ]);
 
-      $predictions = $response->json("predictions");
+    $predictions = $response->json("predictions");
 
-      return $predictions[0];
-    });
+    return $predictions[0];
   }
 }
